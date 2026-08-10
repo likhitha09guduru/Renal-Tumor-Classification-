e19b3f10c1e90be95e4a44ddb0aadd63f1545298
Likhitha <likhitha09guduru@gmail.com>
2026-08-10 15:49:58 +0530

    dvc

--- app.py
+++ app.py
@@ -23,3 +23,31 @@ class ClientApp:
 @cross_origin()
 def home():
     return render_template('index.html')
+
+
+
+
+@app.route("/train", methods=['GET','POST'])
+@cross_origin()
+def trainRoute():
+    os.system("python main.py")
+    # os.system("dvc repro")
+    return "Training done successfully!"
+
+
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    image = request.json['image']
+    decodeImage(image, clApp.filename)
+    result = clApp.classifier.predict()
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+
+    app.run(host='0.0.0.0', port=8080) #for AWS
+
+
